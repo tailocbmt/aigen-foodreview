596f573686746507c731bc4f66ec58f9b8254d25
chiisa <locntse150150@fpt.edu.vn>
2026-03-27 19:06:13 +0800

    update new model

---
+++ evaluate_separate_mem_multimodal.py
@@ -0,0 +1,103 @@
+import torch
+import os
+import json
+from transformers import CLIPProcessor, CLIPModel, FlavaProcessor, FlavaModel
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from torch.utils.data import DataLoader
+from modules.dataset import HintsOfTruthMultimodalDataset, MultimodalDataset
+from larimar_base.exp_models import CLIPDetectorSeparateMemory
+
+# CONFIG
+
+# Define the path to your config file
+config_path = 'configs/multimodal_separate_mem_config.json'
+
+# Open and read the JSON file
+with open(config_path, 'r') as file:
+    config = json.load(file)
+
+# Extract values into variables
+# Options for model_name: 'clip', 'flava'
+model_name = config.get('model_name', 'flava')
+fusion_type = config.get('fusion_type', 'concat')
+# Note: for 'clip', max length should be 77
+dataset = config.get('dataset', 'food_review')
+MAX_LENGTH = config.get('MAX_LENGTH', 512)
+test_file = config.get('test_file', '')
+output_dir = config.get('output_dir', '')
+image_dir = config.get('image_dir', '')
+BATCH_SIZE = config.get('BATCH_SIZE', 16)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+available_models = ['clip', 'flava']
+best_acc = 0
+
+# MODEL SELECTION
+if model_name not in available_models:
+    raise ValueError(f'{model_name} not in {available_models}.')
+
+# WEIGHTS
+weights = os.listdir(output_dir)
+weights = sorted(weights, key=lambda x: int(x.split('-')[1].split('.')[0]))
+weights = weights[-1]
+weights_dir = os.path.join(output_dir, weights)
+
+if model_name == 'clip':
+    backbone = CLIPModel.from_pretrained("openai/clip-vit-base-patch16")
+    processor = CLIPProcessor.from_pretrained('openai/clip-vit-base-patch16')
+    model = CLIPDetectorSeparateMemory(
+        backbone=backbone,
+        processor=processor,
+        embed_dim=512,
+        out_dim=1,
+        use_memory=True,
+        memory_size=512,
+        memory_mode="read",
+        fusion_type=fusion_type,   # or "gated_add"
+    )
+else:
+    pass
+
+model = model.to(device)
+print(f'Model {model_name} loaded at weights: {weights}.')
+
+# DATA
+if dataset == "hints_of_truth":
+    test = HintsOfTruthMultimodalDataset(
+        test_file, image_dir, "test", processor, MAX_LENGTH)
+else:
+    test = MultimodalDataset(test_file, image_dir, processor, MAX_LENGTH)
+
+test_dataloader = DataLoader(test, BATCH_SIZE)
+print(f'Loaded Testing File: {test_file}.')
+
+pred_val = []
+labels_val = []
+
+if hasattr(model, "reset_memory"):
+    model.reset_memory()
+
+model.eval()
+model.memory_mode = "read"   # or "off"
+with torch.no_grad():
+    print('Validating..')
+    for j, batchv in enumerate(test_dataloader):
+        inputs_val = batchv['inputs']
+        inputs_val = {key: tensor.squeeze(1).to(
+            device) for key, tensor in inputs_val.items()}
+        label_val = batchv['label'].numpy().tolist()
+        output_val = model(inputs_val).squeeze(1).to(torch.float64)
+        predictions = torch.sigmoid(output_val)
+        predictions = torch.where(
+            predictions > 0.5, 1, 0).detach().cpu().numpy().tolist()
+        pred_val.extend(predictions)
+        labels_val.extend(label_val)
+
+    acc = accuracy_score(pred_val, labels_val)
+    prec = precision_score(pred_val, labels_val)
+    rec = recall_score(pred_val, labels_val)
+    f1 = f1_score(pred_val, labels_val)
+    print(f'Accuracy on the test set: {acc}')
+    print(f'Precision on the test set: {prec}')
+    print(f'Recall on the test set: {rec}')
+    print(f'F1-score on the test set: {f1}')
