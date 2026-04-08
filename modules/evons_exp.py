2746716e23b7e89c3664687c944e6f61285a4d0e
chiisa <locntse150150@fpt.edu.vn>
2026-04-08 22:58:35 +0800

    update flux exp evon

--- modules/evons_exp.py
+++ modules/evons_exp.py
@@ -176,8 +176,8 @@ def main():
                     prompt=original_text,
                     num_inference_steps=5,
                     guidance_scale=3.5,
-                    height=768,
-                    width=768,
+                    height=512,
+                    width=512,
                     max_sequence_length=256
                 ).images[0]
 
