d792c52940578a98aa21aedd9230611c1f2bc6c8
chiisa <locntse150150@fpt.edu.vn>
2026-04-17 23:55:21 +0800

    update new models

--- modules/evons_exp.py
+++ modules/evons_exp.py
@@ -102,7 +102,7 @@ def main():
             with torch.inference_mode():
                 image_result = pipe(
                     prompt=original_text,
-                    num_inference_steps=4,
+                    num_inference_steps=2,
                     strength=0.5,
                     guidance_scale=0.0,
                     # num_inference_steps=9, guidance_scale=0.0, z_image
