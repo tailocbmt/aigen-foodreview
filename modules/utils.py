2d7945e4cfda80869d71dee5872bd622453c60e0
chiisa <locntse150150@fpt.edu.vn>
2026-04-25 16:25:05 +0800

    update util

--- modules/utils.py
+++ modules/utils.py
@@ -8,9 +8,9 @@ def multilabel_accuracy(targets, preds):
     logits:  Tensor of shape [batch_size, num_labels]
     targets: Tensor of shape [batch_size, num_labels], values 0/1
     """
-    preds = torch.tensor(preds)
-    targets = torch.tensor(targets)
-    targets = targets.int()
+    y_pred = torch.tensor(preds)
+    y_true = torch.tensor(targets)
+    y_true = y_true.int()
 
-    correct = (preds == targets).float()
+    correct = (y_pred == y_true).float()
     return correct.mean().item()
